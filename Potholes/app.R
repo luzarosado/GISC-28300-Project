263dc6f5982c92ddbae472cf8ff3602d7e3ebb45
luzarosado <luzarosado@outlook.com>
2023-11-25 12:33:42 -0600

    Always something fucking up or whatever

--- Potholes/app.R
+++ Potholes/app.R
@@ -1,51 +1,26 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tmap)
+library(sf)
+library(lubridate)
 
-# Define UI for application that draws a histogram
+
+potholes <- read.csv("Potholes_Patched.csv")
+wards <- read_sf("WARDS_2015.shp")
+
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  tmapOutput("mymap")
+  
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output, session) {
+  output$mymap <- renderTmap({
+    tm_shape(wards) +
+    tm_polygons() +
+    tm_shape(potholes)
+    tm_dots()
+    
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyapp(ui, server)
